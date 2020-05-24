64a4713f791e47fd84d1f39689dc8df55eaefb5a
bkorumaz <bahadir.korumaz@gmail.com>
2020-05-24 23:55:35 +0300

    added empty modal for new form creating apge

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import SearchBox from "./components/search-box.component";
 import FormList from "./components/form-list.component";
 import { useSelector, useDispatch } from 'react-redux';
 import { createForm } from './redux/root-reducer'
+import { Modal } from "react-bootstrap";
+
 
 function App() {
+  const [modalShow, setModalShow] = React.useState(false);
   const [searchField, setSearchField] = useState("");
   const forms = useSelector(state => state.formList);
 
@@ -34,13 +37,45 @@ function App() {
               </div>
               <FormList forms={forms} searchField={searchField} />
               <div className="card-footer">
-                <Button variant="btn btn-primary" onClick={() => dispatch(createForm({name: "new form"}))}>Yeni Form Oluştur</Button>
+                <Button variant="btn btn-primary" onClick={() => setModalShow(true)}>Yeni Form Oluştur</Button>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </div>
   );
 }
 export default App;
+
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Yeni Form Oluştur
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Yeni form olutşruma içeriği buraya gelecek
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Kapat</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
