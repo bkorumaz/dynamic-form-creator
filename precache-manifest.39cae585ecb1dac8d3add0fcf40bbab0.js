e1b34950db179ec5ab2d18272e4d01a1d5a21009
bkorumaz <bahadir.korumaz@gmail.com>
2020-05-27 16:07:45 +0300

    Updates

---
+++ precache-manifest.39cae585ecb1dac8d3add0fcf40bbab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8b825460d2da14bc4966a24e20174f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47b62c8cea29e92fec23",
+    "url": "/static/css/main.a86ccfc2.chunk.css"
+  },
+  {
+    "revision": "1a44b82cdedcd70f0772",
+    "url": "/static/js/2.d4d09a40.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d4d09a40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47b62c8cea29e92fec23",
+    "url": "/static/js/main.1aa4688d.chunk.js"
+  },
+  {
+    "revision": "0b675094d4106fa04cc0",
+    "url": "/static/js/runtime-main.8e5f6c83.js"
+  }
+]);
