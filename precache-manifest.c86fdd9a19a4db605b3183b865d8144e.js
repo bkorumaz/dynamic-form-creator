1c13fd61867912a6de4f4483cfa7b84216a515c3
bkorumaz <bahadir.korumaz@gmail.com>
2020-05-27 16:35:41 +0300

    Updates

---
+++ precache-manifest.c86fdd9a19a4db605b3183b865d8144e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0bfbd4fbe73b203720c11df32a5339",
+    "url": "/evam-interview-project/index.html"
+  },
+  {
+    "revision": "c0fd7d463d2e7f45d477",
+    "url": "/evam-interview-project/static/css/main.a86ccfc2.chunk.css"
+  },
+  {
+    "revision": "e9a634b028c886bf8cba",
+    "url": "/evam-interview-project/static/js/2.fbcae5c1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/evam-interview-project/static/js/2.fbcae5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0fd7d463d2e7f45d477",
+    "url": "/evam-interview-project/static/js/main.dc06be68.chunk.js"
+  },
+  {
+    "revision": "b275fab581b97b400642",
+    "url": "/evam-interview-project/static/js/runtime-main.40d68a03.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ea9d5261176a2b279d8f989904d674f.js"
+  "/evam-interview-project/precache-manifest.c86fdd9a19a4db605b3183b865d8144e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/evam-interview-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
